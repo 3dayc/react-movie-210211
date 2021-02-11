b60a4b052519c6bec24218632ab59ff45a966fd7
hoonlocal <hoonlocal@naver.com>
2021-02-11 15:33:55 +0900

    react test02

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <li>t</li>
+      <li>d</li>
+      <li>d</li>
+      <li>s</li>
+      <li>a</li>
+      <li>
+        <a href="http://www.naver.com">링크</a>
+      </li>
+    </ul>
   );
 }
 
